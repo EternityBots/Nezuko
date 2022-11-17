600b558c2ed2a0c6953fc17d632cd4eeaacee132
Ray Senpai <89797110+RaySenpai69@users.noreply.github.com>
2022-11-17 11:59:23 +0530

    Welcoming new era ✨

--- Organs/commands/General/nezuko.js
+++ Organs/commands/General/nezuko.js
@@ -1,4 +1,5 @@
 require('../../../settings')
+const axios = require('axios')
 module.exports = {
     name: "repo",
     alias: ["nezuko"],
@@ -21,9 +22,10 @@ let yup = [
             'https://wallpaperaccess.com/full/1105099.jpg',
             'https://images8.alphacoders.com/120/1206602.png'
           ]
+let nezuko = await axios.get('https://api.github.com/repos/EternityBots/Nezuko')
 let rae = yup[Math.floor(Math.random() * yup.length)]
 
-var rr = `❤️ _*Nezuko made by Eternity*_ ❤️\n\n🍀 *Description:* A simple and easy-to-use WhatsApp bot project based on Multi-Device Baileys and written in JavaScript\n\n 💕 *BOT URL:* https://github.com/EternityBots/Nezuko \n`
+var rr = `✨ _*🅽︎🅴︎🆉︎🆄︎🅺︎🅾︎*_  ✨\n\n🍀 *Description:* ${nezuko.data.description}\n\n 📎 *Repo Link:* ${nezuko.data.html_url} \n\n 🌠 *Total Star:* ${nezuko.data.stargazers_count}\n\n 💕 *Total Forks:* ${nezuko.data.forks} \n\n 💁🏻‍♂️ *Tutorial:* https://www.youtube.com/watch?v=KqgyScOlvV8 \n`
 await client.sendMessage(m.from,{image:{url:rae}, caption:rr},{quoted:m})
     }
 }
