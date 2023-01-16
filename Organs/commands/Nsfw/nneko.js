45e534858df7e4004bc28b1a6a9be6c8bb7fe4c2
RaySenpai69 <anirbanray2007gg@outlook.com>
2023-01-16 12:05:07 +0530

    hi

--- Organs/commands/Nsfw/nneko.js
+++ Organs/commands/Nsfw/nneko.js
@@ -1,32 +1,34 @@
-const axios = require('axios')
-const fs=require("fs")
-require ('../../../settings')
-module.exports={
-    name:"nneko",
-    alias:["nsfwneko"],
-    usage:`${prefa}nneko`,
-    desc:"Gives you random hentai neko image",
-    react:"💦",
+const axios = require("axios");
+const fs = require("fs");
+require("../../../settings");
+module.exports = {
+  name: "nneko",
+  alias: ["nsfwneko"],
+  usage: `${prefa}nneko`,
+  desc: "Gives you random hentai neko image",
+  react: "💦",
 
-    category:"Nsfw",
-    start:async(client,m,{command,prefix,text,args})=>{
-    if(!nsfw.includes(`${m.from}`)) return m.reply('*❌ This not a hentai group pervert*')
+  category: "Nsfw",
+  start: async (client, m, { command, prefix, text, args }) => {
+    if (!nsfw.includes(`${m.from}`))
+      return m.reply("*❌ This not a hentai group pervert*");
 
-
-    let wife = await axios.get('https://api.waifu.pics/nsfw/neko')
+    let wife = await axios.get("https://api.waifu.pics/nsfw/neko");
     let buttons = [
-    {buttonId: `${prefix} nneko`, buttonText: {displayText: '>>'}, type: 1}
-    ]
+      {
+        buttonId: `${prefix} nneko`,
+        buttonText: { displayText: ">>" },
+        type: 1,
+      },
+    ];
 
     let buttonMessage = {
-        image: {url:wife.data.url},
-        caption: `*🥵 NYAAAAAAA*`,
-        footer: `*©Eternity-Team*`,
-        buttons: buttons,
-        headerType: 4
-    }
-    client.sendMessage(m.from, buttonMessage, { quoted: m })
-
-
-    }
-}
+      image: { url: wife.data.url },
+      caption: `*🥵 NYAAAAAAA*`,
+      footer: `*©Eternity-Team*`,
+      buttons: buttons,
+      headerType: 4,
+    };
+    client.sendMessage(m.from, buttonMessage, { quoted: m });
+  },
+};
