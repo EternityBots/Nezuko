28a2b013287075a6668a32566458295644675c11
Ray Senpai <89797110+RaySenpai69@users.noreply.github.com>
2023-02-21 10:24:43 +0530

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -4,12 +4,12 @@
         pkgs.nodejs-16_x
         pkgs.libwebp
         pkgs.python
-	    pkgs.nodePackages.typescript
         pkgs.libuuid
         pkgs.ffmpeg
         pkgs.imagemagick  
         pkgs.wget
         pkgs.git
+        pkgs.yarn
     ];
   env ={
     LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid];
