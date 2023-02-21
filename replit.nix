ba287af0000a82aba6a2c1372f0654e043bf3af1
Ray Senpai <89797110+RaySenpai69@users.noreply.github.com>
2023-02-21 10:35:20 +0530

    Reverting back to Fantox

--- replit.nix
+++ replit.nix
@@ -4,18 +4,16 @@
         pkgs.nodejs-16_x
         pkgs.libwebp
         pkgs.python
+	    pkgs.nodePackages.typescript
         pkgs.libuuid
         pkgs.ffmpeg
         pkgs.imagemagick  
         pkgs.wget
         pkgs.git
-        pkgs.yarn
+        pkgs.nodePackages.pm2
     ];
   env ={
     LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid];
   };
 }
-
-
-
 
