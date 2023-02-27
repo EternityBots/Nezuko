ef34dcc873d68fde5c50cf931927bc5debffe3d1
Ray Senpai <89797110+RaySenpai69@users.noreply.github.com>
2023-02-27 23:34:02 +0530

    Updated Genshin
    Old one wasn't getting support from dev

--- Organs/commands/Weeb/genshin.js
+++ Organs/commands/Weeb/genshin.js
@@ -1,41 +1,35 @@
-const genshin = require("genshin-api")
-require ('../../../settings')
-module.exports = {
-    name: "genshin",
-    alias: ["gchara"],
-    usage: `${prefa}genshin <any genshin characher name>`,
-    desc: "Gives you details of genshin characher.",
-    react: "✅",
-    
-    category: "Weeb",
-    start: async (client, m, {
-        command,
-        prefix,
-        text,
-        args
-    }) => {
+const genshindb = require('genshin-db');
+const fs=require("fs")
+module.exports={
+    name:"genshin",
+    alias:["gchara", "genshinchara"],
+    usage:"=gchara [name]",
+    desc:"Gives you the data of the given genshin character.",
+    category:"Weeb",
+    react:"✅",
+
+    start:async(client,m,{command,prefix,args})=>{
+if (!q) return m.reply(`*Please provide me a genshin character name*`)
 try {
-if (!text) return m.reply(`❌ No query provided!`)
-    a = text.toLowerCase();
-    const anime = await genshin.Characters(text)
-let txt = ""
-txt += `🎀 *Name:* ${anime.name}\n`
-txt += `🎖️ *Title:* ${anime.title}\n`
-txt += `💠 *Vision:* ${anime.vision}\n`
-txt += `🏹 *Weapon:* ${anime.weapon}\n`
-txt += `💮 *Gender:* ${anime.gender}\n`
-txt += `🌏 *Nation:* ${anime.nation}\n`
-txt += `🪷 *Affiliation:* ${anime.affiliation}\n`
-txt += `🌟 *Rarity:* ${anime.rarity}\n`
-txt += `❄️ *Constellation:* ${anime.constellation}\n`
-txt += `📖 *Description:* ${anime.description}\n`
-txt += `🌐 *Url:* https://genshin-impact.fandom.com/wiki/${a}\n`
-
-
-urll = `https://api.genshin.dev/characters/${a}/portrait`
-await client.sendMessage(m.from,{image:{url:urll}, caption:txt},{quoted:m})
+const genshin = await genshindb.characters(q);
+if (!genshin) return m.reply(`Can't find the character data`)
+const reply =
+`💎 *Name:* ${genshin.name}
+💠 *Elemnent:* ${genshin.element}
+📛 *Weapon:* ${genshin.weapontype}
+🎗 *Speciality:* ${genshin.substat}
+🌟 *Rarity:* ${genshin.rarity}
+🌸 *Gender:* ${genshin.gender}
+❄ *Constellation:* ${genshin.constellation}
+⛩ *Region:* ${genshin.region}
+💮 *Affiliation:* ${genshin.affiliation}
+🎁 *Birthday:* ${genshin.birthday}
+💛 *Description:* ${genshin.description}
+🌐 *URL:* ${genshin.url.fandom}`
+await client.sendMessage(m.from,{image:{url:genshin.images.cover2}, caption:reply},{quoted:m})
 } catch (err) {
-console.log(err)
-return m.reply ('⚠️ Something Went Wrong')
-}}
+    console.log(err)
+    return m.reply (`✖ An error occurred. Please try again later.`)
+    }
+  }
 }
