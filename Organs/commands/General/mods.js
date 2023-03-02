a94d29d8ba9ee21e45253fc1bb48296def9c9dfc
Ray Senpai <89797110+RaySenpai69@users.noreply.github.com>
2023-03-02 15:22:31 +0530

    Las update for this week( New mod menu ) 👑✌️

--- Organs/commands/General/mods.js
+++ Organs/commands/General/mods.js
@@ -1,21 +1,32 @@
+require("../../../settings");
+module.exports = {
+  name: "mods",
+  alias: ["mod"],
+  usage: `${prefa}mods`,
+  desc: "shows mods list",
+  category: "General",
+  react: "✅",
 
-require ('../../../settings')
-module.exports={
-    name:"mods",
-    alias:["mod"],
-    usage:`${prefa}mods`,
-    desc:"shows mods list",
-    category:"General",
-    react:"✅",
-
-    start:async(client,m,{})=>{
-        const mod= owner
-        let mo=`
-*🎉 Nezuko Mods 🎉*\n`
-for(let i=0;i<mod.length;i++){
-    const um= await client.username(mod[i]+'@s.whatsapp.net')
-    mo+=`\n✨${i+1}\n*👤 Name:* ${um}\n*📱 Contact:* http://wa.me/+${mod[i].split("@")[0]}\n`
-}
-await  client.sendMessage(m.from,{image:{url:'https://images2.alphacoders.com/104/1044538.png'},caption:mo},{quoted:m})  
+  start: async (client, m, {}) => {
+    const mod = global.owner;
+    const owner = mod.map((mod) => mod + "@s.whatsapp.net");
+    let mo = `
+*🎉 Nᴇᴢᴜᴋᴏ Mᴏᴅs 🎉*\n`;
+    for (let i = 0; i < mod.length; i++) {
+      const um = await client.username(mod[i] + "@s.whatsapp.net");
+      mo += `\n*👑 Name:* ${um}\n*🚀 Contact:* @${mod[i].split("@")[0]}\n`;
     }
-}
+    let urlll = [
+      "https://static.moewalls.com/videos/preview/2022/neko-nezuko-kimetsu-no-yaiba-preview.mp4",
+      "https://static.moewalls.com/videos/preview/2022/nezuko-lying-on-the-water-kimetsu-no-yaiba-preview.mp4",
+      "https://static.moewalls.com/videos/preview/2022/nezuko-kamado-demon-slayer-preview.mp4",
+      "https://upcdn.io/FW25b6N/raw/y2mate.com%20-%20%20Kamado%20Nezuko%20%20%20Kimetsu%20no%20Yaiba%20%20%20Wallpaper%20Live%204K%20%20%20Wallpaper%20Engine%20%20%20Demon%20slayer%20_360p%20(1).mp4",
+    ];
+    let rae = urlll[Math.floor(Math.random() * urlll.length)];
+    await client.sendMessage(
+      m.from,
+      { video: { url: rae }, gifPlayback: true, caption: mo, mentions: owner },
+      { quoted: m }
+    );
+  },
+};
