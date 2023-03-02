5f0865911aae6a01aee37f4597aed8e864e2651c
Neko Kun <123672103+NekoSenpai69@users.noreply.github.com>
2023-03-02 19:43:39 +0530

    Create Safeoppai.js

---
+++ Organs/commands/Weeb/Safeoppai.js
@@ -0,0 +1,43 @@
+const axios = require('axios')
+
+module.exports = {
+    name: "soppai",
+    alias: ["safeboobs", "sboobs", "safeoppai"],
+    usage: `${prefa}neko`,
+    desc: "Gives you random safe oppai image",
+    react: "😜",
+    
+    category: "Weeb",
+    start: async (client, m, {
+        command,
+        prefix,
+        text
+        
+    }) => {
+        let oppai = await axios.get('https://nekopai-apis-main.onrender.com/oppai')
+        let buttons = [
+            {
+                buttonId: `${prefix} soppai,
+                buttonText: {
+                    displayText: '>>'
+                },
+                type: 1
+            }
+    ]
+        
+        let buttonMessage = {
+            image: {
+                url: oppai.data.url
+            },
+            caption: `*Oppai is love!!*`,
+            footer: `*©Eternity-Team*`,
+            buttons: buttons,
+            headerType: 4
+        }
+        client.sendMessage(m.from, buttonMessage, {
+            quoted: m
+        })
+        
+        
+    }
+}
