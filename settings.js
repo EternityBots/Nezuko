4149d0273c6f9cab73b6d45661b03bc17fd3c9fe
Lakshantharu <120786465+Lakshantharu@users.noreply.github.com>
2023-06-13 04:28:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,11 +7,11 @@ if (!gg) {
 }
 
 global.owner = gg.split(",");
-global.mongodb = process.env.MONGODB || "none";
+global.mongodb = process.env.MONGODB || "mongodb+srv://tiktoktharusha2:<password>@cluster0.qb0imql.mongodb.net/?retryWrites=true&w=majority";
 global.prefa = process.env.PREFIX || ".";
-global.ggle = process.env.GOOGLE_API || "none";
-global.wter = process.env.WEATHER_API || "none";
-global.sessionId = process.env.SESSION_ID || "NEZUKO";
+global.ggle = process.env.GOOGLE_API || "AIzaSyDCfwdaHmH1mTNh2WJNg3wFwJnjLAlNCfY";
+global.wter = process.env.WEATHER_API || "2238e510368b441ea5c222257231206";
+global.sessionId = process.env.SESSION_ID || "tharu";
 global.malid = process.env.MAL_USERNAME || undefined;
 global.malpass = process.env.MAL_PASSWORD || undefined;
 global.port = process.env.PORT || 8080;
